4e064407ee2025b5b1ea7c87e5480105e1baeb0d
Wali-Muhammad09 <walimps6112@gmail.com>
2026-01-31 17:56:52 +0500

    Update app.py

--- app.py
+++ app.py
@@ -147,7 +147,7 @@ Crate("Mythic","4 USDT")
 
 # ---------------- HEADER ----------------
 st.markdown("<div class='title'>🔥 PKSMP STORE 🔥</div>",unsafe_allow_html=True)
-st.markdown("<div class='subtitle'>USDT is Crypto Currency | Search USDT to <your currency> in your browser for price</div>",unsafe_allow_html=True)
+st.markdown("<div class='subtitle'>USDT is Crypto Currency | Search USDT to 'your currency' in your browser for price</div>",unsafe_allow_html=True)
 store=st.radio("Choose Category",["Ranks","Crate Keys"])
 
 # ---------------- EMAIL PURCHASE SYSTEM ----------------
@@ -238,5 +238,6 @@ if store=="Crate Keys":
             """,unsafe_allow_html=True)
 
             purchase(c.name+" Crate Key")
+
 
 
